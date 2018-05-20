64ff3c9cfa759b4052a960881bc5426256e3ee32
vincent-karuri <vincentmkaruri@gmail.com>
2018-05-20 09:05:09 +0000

    Add playbooks
    1. Playbooks that create an Ubuntu Linux machine in Azure
    2. Playbooks that deploy a PYthon app on the machine

---
+++ deploy-app-playbooks/create-path-variable.sh
@@ -0,0 +1,5 @@
+echo "export PATH=/usr/local/anaconda/bin:/home/vkaruri/.conda/envs:"$PATH >> ~/.bashrc
+echo "export PATH=/usr/local/anaconda/bin:/home/vkaruri/.conda/envs:"$PATH >> ~/.profile
+echo "export PATH=/usr/local/anaconda/bin:/home/vkaruri/.conda/envs:"$PATH >> /etc/environment
+source ~/.bashrc
+source ~/.profile
